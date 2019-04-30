9d787d839abf440916bf38b016d902ec0154640e
Bastiaan Olij <mux213@gmail.com>
2019-04-30 21:51:46 +1000

    Sync with godot master 20190430

--- src/ARVRInterface.cpp
+++ src/ARVRInterface.cpp
@@ -548,6 +548,10 @@ int godot_arvr_get_external_texture_for_eye(void *p_data, int p_eye) {
 	return 0;
 }
 
+void godot_arvr_notification(int p_what) {
+	// nothing to do here for now but we should implement this.
+}
+
 const godot_arvr_interface_gdnative interface_struct = {
 	GODOTVR_API_MAJOR, GODOTVR_API_MINOR, 
 	godot_arvr_constructor,
@@ -566,5 +570,6 @@ const godot_arvr_interface_gdnative interface_struct = {
 	godot_arvr_commit_for_eye,
 	godot_arvr_process,
 	// only available in Godot 3.2+
-	godot_arvr_get_external_texture_for_eye
+	godot_arvr_get_external_texture_for_eye,
+	godot_arvr_notification
 };
